5b387030a822423116d90f1ee389eb34e6d786c8
VPresich <valya.alexandrova.presich@gmail.com>
2024-04-17 08:11:48 +0200

    add functions for contacts and invokeAction function

---
+++ contacts.js
@@ -0,0 +1,57 @@
+const fs = require('node:fs/promises');
+const path = require('node:path');
+const { v4: uuidv4 } = require('uuid');
+
+const contactsPath = path.join(__dirname, 'contacts.json');
+
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, { encoding: 'utf-8' });
+    return JSON.parse(data);
+  } catch (error) {
+    return [];
+  }
+}
+
+async function getContactById(contactId) {
+  try {
+    const contacts = await listContacts();
+    return contacts.find(contact => contact.id === contactId) || null;
+  } catch (error) {
+    return null;
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const contacts = await listContacts();
+    const index = contacts.findIndex(contact => contact.id === contactId);
+    if (index !== -1) {
+      const removedContact = contacts.splice(index, 1)[0];
+      await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+      return removedContact;
+    }
+    return null;
+  } catch (error) {
+    return null;
+  }
+}
+
+async function addContact(name, email, phone) {
+  try {
+    const newContact = { id: uuidv4(), name, email, phone };
+    const contacts = await listContacts();
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return newContact;
+  } catch (error) {
+    return null;
+  }
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
---
+++ index.js
@@ -0,0 +1,52 @@
+const { program } = require('commander');
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require('./contacts');
+
+program
+  .option('-a, --action <type>', 'choose action: list, get, remove, add')
+  .option('-i, --id <type>', 'contact id')
+  .option('-n, --name <type>', 'contact name')
+  .option('-e, --email <type>', 'contact email')
+  .option('-p, --phone <type>', 'contact phone');
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case 'list':
+      const dataArr = await listContacts();
+      if (dataArr.length) {
+        console.table(dataArr);
+      } else {
+        console.table('The file is empty');
+      }
+      break;
+
+    case 'get':
+      console.log(await getContactById(id));
+      break;
+
+    case 'remove':
+      console.log(await removeContact(id));
+      break;
+
+    case 'add':
+      console.log(await addContact(name, email, phone));
+      break;
+
+    case 'update':
+      console.log(await addContact(name, email, phone));
+      break;
+
+    default:
+      console.warn('\x1b[31m Unknown action type!');
+  }
+}
+
+invokeAction(argv);
